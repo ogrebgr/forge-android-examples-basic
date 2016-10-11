5accb7c976ef4a14a0141974487d64d824956fae
Ognyan Bankov <ogibankov@gmail.com>
2016-10-11 09:51:34 +0300

    cosmetics

--- app/src/main/java/com/bolyartech/forgeexamples/basic/UnitBaseActivity.java
+++ app/src/main/java/com/bolyartech/forgeexamples/basic/UnitBaseActivity.java
@@ -10,7 +10,8 @@ import com.bolyartech.forge.android.app_unit.UnitActivity;
 /**
  * Your unit activities will have to inherit this base class
  *
- * UnitBaseActivity is not included in the library because you may need to chose between AppCompatActivity and Activity
+ * UnitBaseActivity is not included in the library because you may need to chose between AppCompatActivity and Activity.
+ * You may have more than one unit base activity depending on your needs.
  */
 abstract public class UnitBaseActivity<T extends ResidentComponent> extends AppCompatActivity
         implements UnitActivity<T> {
