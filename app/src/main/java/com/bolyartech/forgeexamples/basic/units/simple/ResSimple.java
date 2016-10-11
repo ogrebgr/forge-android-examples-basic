27d9c6207bf536fcd163f4401d0412b0def7570e
Ognyan Bankov <ogibankov@gmail.com>
2016-10-11 10:28:16 +0300

    removed Act Simple

--- app/src/main/java/com/bolyartech/forgeexamples/basic/units/simple/ResSimple.java
+++
@@ -1,7 +0,0 @@
-package com.bolyartech.forgeexamples.basic.units.simple;
-
-import com.bolyartech.forge.android.app_unit.ResidentComponentAdapter;
-
-
-class ResSimple extends ResidentComponentAdapter {
-}
