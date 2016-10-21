79045fef147516f6e7c3b7c55ed9c66cd4ff7818
Ognyan Bankov <ogibankov@gmail.com>
2016-10-21 16:38:21 +0300

    access level optimization

--- app/src/main/java/com/bolyartech/forgeexamples/basic/units/realistic/ResRealistic.java
+++ app/src/main/java/com/bolyartech/forgeexamples/basic/units/realistic/ResRealistic.java
@@ -3,7 +3,7 @@ package com.bolyartech.forgeexamples.basic.units.realistic;
 import com.bolyartech.forge.android.app_unit.ResidentComponent;
 
 
-public interface ResRealistic extends ResidentComponent {
+interface ResRealistic extends ResidentComponent {
     void retrieveExampleData();
     ExampleData getExampleData();
 }
